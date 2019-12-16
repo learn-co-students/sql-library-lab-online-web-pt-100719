c810a5a31482cc2b42decc56772ce9696dcf5712
Santiago Salazar <santiago.s.pavajeau@gmail.com>
2019-12-16 02:04:30 +0000

    Done.

--- lib/querying.rb
+++ lib/querying.rb
@@ -1,24 +1,42 @@
 def select_books_titles_and_years_in_first_series_order_by_year
-  "Write your SQL query here"
+  "SELECT title, year FROM books
+  WHERE series_id = 1
+  ORDER BY year;"
 end
 
 def select_name_and_motto_of_char_with_longest_motto
-  "Write your SQL query here"
+  "SELECT name, motto FROM characters
+  ORDER BY LENGTH(motto) DESC 
+  limit 1;"
 end
 
 
 def select_value_and_count_of_most_prolific_species
-  "Write your SQL query here"
+  "SELECT species, COUNT(species) FROM characters
+  GROUP BY species ORDER BY COUNT(species) DESC
+  LIMIT 1"
 end
 
 def select_name_and_series_subgenres_of_authors
-  "Write your SQL query here"
+  "SELECT authors.name, subgenres.name FROM subgenres
+  LEFT OUTER JOIN series ON subgenres.id = series.subgenre_id
+  LEFT OUTER JOIN authors ON authors.id = series.author_id;"
 end
 
 def select_series_title_with_most_human_characters
-  "Write your SQL query here"
+  "SELECT series.title FROM series
+  LEFT OUTER JOIN authors ON authors.id = series.author_id
+  LEFT OUTER JOIN characters ON characters.author_id = authors.id
+  WHERE characters.species = 'human'
+  GROUP BY series.title
+  ORDER BY COUNT(characters.species = 'human') DESC
+  LIMIT 1;"
 end
 
 def select_character_names_and_number_of_books_they_are_in
-  "Write your SQL query here"
+  "SELECT characters.name, COUNT(books.title) FROM characters
+  LEFT OUTER JOIN character_books ON character_books.character_id = characters.id
+  LEFT OUTER JOIN books ON books.id = character_books.book_id
+  GROUP BY characters.name
+  ORDER BY COUNT (books.title) DESC;"
 end
