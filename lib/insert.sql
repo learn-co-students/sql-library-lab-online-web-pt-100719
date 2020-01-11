b22744f45e4dd37de556f271c4701327de06736e
nicmlu <NicoleMLucien@gmail.com>
2020-01-11 17:19:50 -0500

    insert 2 series and 6 books data into tables

---
+++ lib/insert.sql
@@ -0,0 +1,8 @@
+INSERT INTO series (title) VALUES ("Harry Potter");
+INSERT INTO series (title) VALUES ("Percy Jackson");
+INSERT INTO books (title, year, series_id) VALUES ("Harry Potter and the Sorcerer's Stone", 1997, 1);
+INSERT INTO books (title, year, series_id) VALUES ("Harry Potter and the Chamber of Secrets", 1998, 1);
+INSERT INTO books (title, year, series_id) VALUES ("Harry Potter and the Prisoner of Azkaban", 1999, 1);
+INSERT INTO books (title, year, series_id) VALUES ("The Lightening Thief", 2005, 2);
+INSERT INTO books (title, year, series_id) VALUES ("The Titan's Curse", 2007, 2);
+INSERT INTO books (title, year, series_id) VALUES ("The Battle of Labyrinth", 2008, 2);
